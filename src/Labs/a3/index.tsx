d92b6f846a0e08ee0f36ef79ac40ec8eaf533525
jjviasus <61569318+jjviasus@users.noreply.github.com>
2024-02-17 16:27:58 -0500

    lab completed

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -4,11 +4,17 @@ import PathParameters from "./routing/PathParameters";
 import Styles from "./Styles";
 import ConditionalOutput from "./ConditionalOutput";
 import Highlight from "./Highlight";
+import Add from "./Add";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
 
 function Assignment3() {
     return (
         <div>
             <h1>Assignment 3</h1>
+            <TodoItem/>
+            <TodoList/>
+            <Add a={3} b={4} />
             <Highlight>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe
                 totam
